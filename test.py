96283a052a083bb33c396b928d3acc7d8619eaf8
Xiang-Li Li <1905692338@qq.com>
2022-02-17 19:55:08 +0800

    Update test.py
    update parser

--- test.py
+++ test.py
@@ -251,7 +251,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='test.py')
     parser.add_argument('--use_v3', action="store_true", help='use yolov3 or not')  
     parser.add_argument('--cfg', type=str, default='configs/yolov5s.yaml', help='source')  
-    parser.add_argument('--weights', nargs='+', type=str, default='weights/yolov5s.pkl', help='model.pt path(s)')
+    parser.add_argument('--weights',type=str, default='weights/yolov5s.pkl', help='model.pt path(s)')
     parser.add_argument('--data', type=str, default='data/coco128.yaml', help='*.data path')
     parser.add_argument('--batch-size', type=int, default=32, help='size of each image batch')
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
